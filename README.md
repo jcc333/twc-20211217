ab2f97e3d418dd0502533b21f09919d0572dd0e3
Emily Reynolds <emilyreynoldsdsa@gmail.com>
2021-12-18 15:41:59 -0500

    Texting with Twilio, Env and gitignore added.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # twc-20211217
 Don't worry about it.
+
+# Inspiration 
+https://github.com/SeanDaBlack/KelloggBot/pulls 
+
+# Aspire Site
+https://aspire-together.org/contact-us/
--- script.py
+++ script.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv 
+
+#Load environmental variables
+load_dotenv() 
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.getenv('SID')
+auth_token = os.getenv('Auth')
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Hi, this is a test",
+                     from_='+13306484254',
+                     to='+19152675166'
+                 )
+
+print(message.sid)
